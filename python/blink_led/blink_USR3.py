d11e97a8e2746c9e1ffda54236902861c435903f
lulushih2 <ys95@rice.edu>
2024-02-20 19:48:48 -0600

    Assignment 4

---
+++ python/blink_led/blink_USR3.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+--------------------------------------------------------------------------
+Blink USR3
+--------------------------------------------------------------------------
+License:   
+Copyright 2024 - Lulu Shih
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Author: Lulu Shih
+Date: February 18th, 2024
+
+Blink LED on PocketBeagle
+
+This script blinks the USR3 LED using the Adafruit_BBIO library.
+
+--------------------------------------------------------------------------
+"""
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+# Setting up the LED as an output
+LED = "USR3"
+GPIO.setup(LED, GPIO.OUT)
+
+# Setting frequency of blinking
+frequency = 5
+delay = 0.5/frequency
+
+# Main loop to blink the LED
+while True:
+    GPIO.output(LED, 1)
+    time.sleep(delay)
+    GPIO.output(LED, 0)
+    time.sleep(delay)
+    
+# End of loop
+    
